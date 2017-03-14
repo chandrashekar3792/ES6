719fea5d109a5a187f5e6b9ab6ada9e196932260
Chandrashekar <chandrashekar.s@medibox.in>
2017-03-15 01:20:05 +0530

    Javascript classes and how to inherit the functions within a particular class

---
+++ classes.js
@@ -0,0 +1,41 @@
+"use strict";
+
+class Person {
+  constructor(age,height){
+    this.age=age;
+    this.height=height;
+    console.log('age is',this.age,'height is',this.height);
+  }
+// Get Method
+  get agePlus(){
+    return this.age + this.height;
+  }
+}
+
+var person = new Person(28,6);
+console.log(person.agePlus);
+
+// Example two
+
+class Cat{
+  constructor(name){
+    this.name = name;
+  }
+
+  speak(){
+    console.log(this.nam + 'makes noise');
+  }
+}
+
+var cat= new Cart('Meow');
+cat.speak();
+
+class Lion extends Cat {
+  // function overriding
+  speak(){
+    super.speak();
+    console.log(this.name+'roars');
+  }
+}
+
+var meow = new Lion('meow');
