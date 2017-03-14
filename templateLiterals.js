84324645e1d231f6f612e228f79aff8907542c39
Chandrashekar <chandrashekar.s@medibox.in>
2017-03-15 01:01:51 +0530

    Javascript Template literals

---
+++ templateLiterals.js
@@ -0,0 +1,14 @@
+console.log("hello\n"+"world");
+
+//ES6
+console.log(`hello`
+            'world');
+
+
+
+// O/p:hello
+     //world
+
+var foo='bar';
+var baz='bar';
+console.log(`the first thing is ${foo} and second thing is ${baz});
