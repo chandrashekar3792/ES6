2ee4b327882b479c1dd1663dc720161507ccfe87
Chandrashekar <chandrashekar.s@medibox.in>
2017-03-16 00:04:34 +0530

    javascript another new data type weakmaps

---
+++ weakMaps.js
@@ -0,0 +1,21 @@
+var myMap = new Map();
+
+var myWeakMap = new WeakMap();
+
+var object1 = {'foo':'bar'}
+var object1 = {'bar':'baz'}
+
+myMap.set(object1,'hello');
+myWeakMap.set(object2,'hello');
+
+//--similarities
+console.log(myMap.get(object1));//hello
+console.log(myWeakMap.get(object2))//hello
+//---diffrence
+object1=null;
+object2=null;
+
+myMap.forEach(function(key,val){
+  console.log(key,val);//foo,bar
+});
+myWeakMap.get(object2);//undifined
