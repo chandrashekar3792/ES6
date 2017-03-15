41026849e35cca90e407d2edf5f86f75a5d0daeb
Chandrashekar <chandrashekar.s@medibox.in>
2017-03-15 21:00:40 +0530

    javascript new data type symbols

---
+++ symbols.js
@@ -0,0 +1,17 @@
+//javascript data types
+    //1-undefined
+    //2-null
+    //3-boolean
+    //4-number
+    //5-string
+    //6-object //Array
+// ES6 new data type
+    //7-Symbols
+    //Symbols gives discription to some variable 
+
+var sym=Symbol("foo");
+console.log(typeof sym);//Symbol
+
+console.log(Symbol("foo")===Symbol("foo"));//false
+
+console.log(Number(3)===Number(3));//true
