88a227affbe1b825916ab9bbbe03f6f03c29678b
Chandrashekar S <chandrashekar998@gmail.com>
2017-04-18 00:25:51 +0530

    Adding an example of constant variables

---
+++ constants.js
@@ -0,0 +1,8 @@
+// Constants also known as "immutable variables"
+// I.E A variable which cannot be re-assigned new contenr
+
+const PI=3.141593
+
+console.log(PI>3.0);
+ PI=4;
+ console.log('PI',PI);//TypeError: Assignment to constant variable.
