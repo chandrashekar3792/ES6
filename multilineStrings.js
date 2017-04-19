be049abff3edd05bd95072e04c55eee5bcdcb836
Chandrashekar S <chandrashekar998@gmail.com>
2017-04-20 00:17:32 +0530

    Multiple line string

---
+++ multilineStrings.js
@@ -0,0 +1,23 @@
+var HTML5_SKELETON =
+'<!doctype html>\n' +
+'<html>\n' +
+'<head>\n' +
+'
+<meta charset="UTF-8">\n' +
+'
+<title></title>\n' +
+'</head>\n' +
+'<body>\n' +
+'</body>\n' +
+'</html>\n';
+
+const HTML5_SKELETON = `
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+</head>
+<body>
+</body>
+</html>`;
