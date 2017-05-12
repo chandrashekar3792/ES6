600919b12016ffb1a1587a7924971cf753822eaf
Chandrashekar S <chandrashekar998@gmail.com>
2017-05-12 23:57:30 +0530

    Update block_scoping.js

--- block_scoping.js
+++ block_scoping.js
@@ -2,15 +2,15 @@
 //Variable Hoisting
 // -> let will not hoist to the entire scope of bock they appear in
 //By contrast var could hoist as below
-// {
-//   console.log(c); // undefined. Due to hoisting
-//   var c = 2;
-// }
+{
+  console.log(c); // undefined. Due to hoisting
+  var c = 2;
+}
 //
-// {
-//   console.log(b); // ReferenceError: b is not defined
-//   let b = 3;
-// }
+{
+  console.log(b); // ReferenceError: b is not defined
+  let b = 3;
+}
 
 function varLoop() {
   for (var i = 0; i < 5; ++i) {
