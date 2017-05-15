8e9629763994cafe642da7fa69cd18ad17fcd2ff
Chandrashekar S <chandrashekar998@gmail.com>
2017-05-16 00:26:24 +0530

    Hoisting erros

---
+++ es6ForEveryOne/hoisting.js
@@ -0,0 +1,8 @@
+console.log(pizza);//undefined
+var pizza ='Deep Dish';
+
+console.log(pizza);//ReferenceError: pizza is not defined
+const pizza ='Deep Dish';
+
+console.log(pizza);//ReferenceError: pizza is not defined
+let pizza ='Deep Dish';
