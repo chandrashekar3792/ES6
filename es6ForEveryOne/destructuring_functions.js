814c2601f6ab18547806a6bd6008b1f6feb82b8a
Chandrashekar S <chandrashekar998@gmail.com>
2017-05-25 00:25:43 +0530

    Destrucing functions

---
+++ es6ForEveryOne/destructuring_functions.js
@@ -0,0 +1,14 @@
+function convertCurrency(amount){
+  const converted={
+    USD:amount*0.76,
+    GPB:amount*0.06,
+    AUD:amount*0.23,
+    MEX:amount*0.16,
+    IND:amount*0.54,
+  };
+  return converted;
+}
+
+const {USD,GPB,AUD,MEX,IND}=convertCurrency(100);
+
+console.log(USD,GPB,AUD,MEX,IND);
