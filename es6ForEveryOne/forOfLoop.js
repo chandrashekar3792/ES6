080cac142fa5740c4e7bb00dc2702907e852ab79
Chandrashekar S <chandrashekar998@gmail.com>
2017-05-26 00:20:12 +0530

    For of loop with index and value of a particular each element of array and applying for loop in arrays

--- es6ForEveryOne/forOfLoop.js
+++ es6ForEveryOne/forOfLoop.js
@@ -15,9 +15,29 @@ for( const index in cuts){
   console.log(cuts[index]);
 }
 
+// for getting array values
 for(const cut of cuts){
   console.log(cut);
   if(cut==='brisket'){
     break;
   }
 }
+
+// if we want index along with values
+for( const cut of cuts.entries()){
+  console.log(cut[0],cut[1]);//cut[0] is index,cut[1] is value
+}
+
+for([i,cut] of cuts.entries()){
+  console.log(`${cut} is the ${i} item`);
+}
+
+function addUpNumber(){
+  let total=0;
+  for(num of arguments){
+    total+=num;
+  }
+  return total;
+}
+
+addUpNumber(10,23,52,34,12,13,123);
