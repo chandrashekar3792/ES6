46e697a388c9b3d78b5dbf9195433056d6e7e849
Chandrashekar S <chandrashekar998@gmail.com>
2017-05-26 00:26:27 +0530

    For of loop with index and value for objects

---
+++ es6ForEveryOne/forOfLoopObjects.js
@@ -0,0 +1,12 @@
+const apple={
+  color:'Red',
+  sie:'Medium',
+  weight:50,
+  sugar:10
+};
+
+for(const prop of Object.keys(apple)()){
+  console.log(prop);//keys
+  const value=apple[prop];
+  console.log(value);//values
+}
