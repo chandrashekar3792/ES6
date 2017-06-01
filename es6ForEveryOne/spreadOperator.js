c124df6c968e53f25fbc60254932e73e9222e30c
Chandrashekar S <chandrashekar998@gmail.com>
2017-06-01 23:57:30 +0530

    Spread operator example

---
+++ es6ForEveryOne/spreadOperator.js
@@ -0,0 +1,9 @@
+const name='chandrashekar';
+console.log(...name);//'c','h','a','n','d','r','a','s','h','e','k','a','r'
+
+const features=['name','age','city'];
+const specially=['Web','desktop','hardware'];
+
+//Concat both arrays
+ const props=[...features,...specially];
+ 
