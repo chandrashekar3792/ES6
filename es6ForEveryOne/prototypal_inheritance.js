f7916eee2800bc8279d9ed64e2901d3870337b46
Chandrashekar S <chandrashekar998@gmail.com>
2017-06-12 22:51:53 +0530

    Enhancements in prototype inheritance

--- es6ForEveryOne/prototypal_inheritance.js
+++ es6ForEveryOne/prototypal_inheritance.js
@@ -7,7 +7,7 @@ function Dog(name,breed){
 }
 
 Dog.prototype.bark=function(){
-  console.log(`Bark Bark! My name is ${this.name}`);
+  console.log(`Bark Bark! My name is`);
 }
 
 Dog.prototype.cuddle=function(){
@@ -15,7 +15,14 @@ Dog.prototype.cuddle=function(){
 }
 
 const snickers= new Dog('Snickers','King Charles');
+const srs= new Dog('Snickers','King Charles');
 console.log('snickers',snickers);
 snickers.bark();
 snickers.cuddle();
 //Once create an new Object,the functions created using prototype called automatically
+
+snickers={
+  name:Snickers,
+  breed:King Charles
+}
+snickers.bark();
